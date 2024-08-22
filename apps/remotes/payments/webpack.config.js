5fb426cbb75555273c06fc0cbe3af472a1b4c0ac
Eluri Krirshna Sai Rahul <erahul@ariqt.com>
2024-08-22 12:15:35 +0530

    Configured the plugins for hosts and remotes

---
+++ apps/remotes/payments/webpack.config.js
@@ -0,0 +1,60 @@
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const mf = require("@angular-architects/module-federation/webpack");
+const path = require("path");
+const share = mf.share;
+
+const sharedMappings = new mf.SharedMappings();
+sharedMappings.register(
+  path.join(__dirname, '../../../tsconfig.base.json'),
+  [/* mapped paths to share */]);
+
+module.exports = {
+  output: {
+    uniqueName: "payments",
+    publicPath: "auto"
+  },
+  optimization: {
+    runtimeChunk: false
+  },
+  resolve: {
+    alias: {
+      ...sharedMappings.getAliases(),
+    }
+  },
+  experiments: {
+    outputModule: true
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+        library: { type: "module" },
+
+        // For remotes (please adjust)
+        // name: "payments",
+        // filename: "remoteEntry.js",
+        // exposes: {
+        //     './Component': './apps/remotes/payments/src/app/app.component.ts',
+        // },
+
+        // For hosts (please adjust)
+        // remotes: {
+        //     "eCommerce": "http://localhost:5200/remoteEntry.js",
+        //     "primeVideo": "http://localhost:4200/remoteEntry.js",
+        //     "orders": "http://localhost:4201/remoteEntry.js",
+        //     "products": "http://localhost:4202/remoteEntry.js",
+        //     "profile": "http://localhost:4200/remoteEntry.js",
+
+        // },
+
+        shared: share({
+          "@angular/core": { singleton: true, strictVersion: true, requiredVersion: 'auto' },
+          "@angular/common": { singleton: true, strictVersion: true, requiredVersion: 'auto' },
+          "@angular/common/http": { singleton: true, strictVersion: true, requiredVersion: 'auto' },
+          "@angular/router": { singleton: true, strictVersion: true, requiredVersion: 'auto' },
+
+          ...sharedMappings.getDescriptors()
+        })
+
+    }),
+    sharedMappings.getPlugin()
+  ],
+};
